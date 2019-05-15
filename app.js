504b8eb4597082717e77d84783ebb99faae6fe6b
Chiara Ripanti <chiararipanti@MacBook-Air-di-Chiara.local>
2019-05-15 23:48:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+
+const location = process.argv[2]
+
+if(!location){
+    console.log("Please provide a location")
+}
+else{
+    geocode(location, (error, {latitude, longitude, location})  => {
+        if(error){
+            return console.log("Error " + error)
+        }
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return console.log("Error " + error)
+            }
+    
+            console.log(location)
+            console.log(forecastData)
+          })
+    })
+}
